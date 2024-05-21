606ec8ccc7e2bad91c89f1c5fad63a801cf535cc
l11040 <113540683+l11040@users.noreply.github.com>
2024-05-21 09:41:00 +0900

    Update : 구현 1

---
+++ Assets/Scripts/InputRebinder.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputRebinder : MonoBehaviour
+{
+    public InputActionAsset actionAsset;
+    private InputAction spaceAction;
+
+    void Start()
+    {
+        spaceAction = actionAsset["Space"];
+    }
+
+    [ContextMenu("Rebind")]
+    public void RebindSpaceToEscape()
+    {
+        if (spaceAction == null)
+            return;
+
+        var bindingIndex = spaceAction.GetBindingIndexForControl(Keyboard.current.spaceKey);
+        if (bindingIndex != -1)
+        {
+            // spaceAction.ChangeBinding(bindingIndex).Erase();
+            // spaceAction.AddBinding("<Keyboard>/excape");
+            // spaceAction.ChangeBinding(0);
+            spaceAction.ApplyBindingOverride(bindingIndex, "<Keyboard>/excape");
+        }
+
+    }
+
+    void OnDestroy()
+    {
+        // 액션을 비활성화합니다.
+        spaceAction?.Disable();
+    }
+
+    void OnSpace()
+    {
+        Debug.Log("test");
+    }
+}
